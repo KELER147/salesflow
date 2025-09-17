31b62f7fff596d1554b69d9538a002a714658792
Leonardo H. Keler <leonardo.keler.14@gmail.com>
2025-09-17 09:02:38 -0300

    feat: add Brand and Category entities and configure database api-stock

---
+++ API-Stock/salesflow-api-stock/src/main/java/leonardo_keler/salesflow_api_stock/entity/Brand.java
@@ -0,0 +1,21 @@
+package leonardo_keler.salesflow_api_stock.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "brands")
+public class Brand {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String name;
+}
---
+++ API-Stock/salesflow-api-stock/src/main/java/leonardo_keler/salesflow_api_stock/entity/Category.java
@@ -0,0 +1,24 @@
+package leonardo_keler.salesflow_api_stock.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String name;
+
+    @Column(length = 200)
+    private String description;
+}
